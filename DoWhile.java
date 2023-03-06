09ec4b434309f4885dbcfafe0fd8bb79c5c730ac
Sanjay Rathour <117800972+sanjayrathou@users.noreply.github.com>
2023-03-06 12:09:15 +0530

    Add files via upload

---
+++ DoWhile.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class DoWhile {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int i=1;
+        do{
+            System.out.println(n*i);
+            i++;
+        }while(i<11);
+    }
+}
