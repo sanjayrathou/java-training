b07e6b5dee9294d6c89d8531aef25c0b6ba3ccbe
Sanjay Rathour <117800972+sanjayrathou@users.noreply.github.com>
2023-03-06 12:08:54 +0530

    Add files via upload

---
+++ ContinueKeyword.java
@@ -0,0 +1,19 @@
+public class ContinueKeyword {
+    public static void main(String[] args) {
+        for(int num=1;num<=50;num++){
+            if(num%3==0) {
+                continue;
+            }System.out.println(num);
+        }
+    /**  int num=1;
+       while(num<=50){
+           if(num%3==0){
+               num++;
+               continue;
+
+           }
+           System.out.println(num);
+           num++;*/
+    }
+}
+
