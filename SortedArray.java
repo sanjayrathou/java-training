85078f88075b032b3d76834a8c3e0615da3f822f
Sanjay Rathour <117800972+sanjayrathou@users.noreply.github.com>
2023-03-10 11:53:03 +0530

    Add files via upload

---
+++ SortedArray.java
@@ -0,0 +1,33 @@
+
+import java.util.*;
+
+public class SortedArray {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int size = sc.nextInt();
+
+        int arr[]=new int[size];
+
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+
+        boolean isAscending=true;
+
+        for(int i=0;i<arr.length-1;i++){
+
+            if(arr[i]>arr[i+1]){
+                isAscending=false;
+            }
+        }
+        if(isAscending){
+            System.out.println("array is sorted in ascending order");
+        }else{
+            System.out.println("the array is not sorted in ascending order");
+        }
+
+
+    }
+}
