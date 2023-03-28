1f52a758a8860aa37dbcbab46fd440c07834e307
Sanjay Rathour <117800972+sanjayrathou@users.noreply.github.com>
2023-03-29 00:13:23 +0530

    Update PatternMatching.java

--- PatternMatching.java
+++ PatternMatching.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class PatternMatching {
     public static void main(String[] args) {
         String str = "ABCABCD";
-        String pattern = "ABCD";
+        String pattern = "CDA";
         for(int i = 0;i<=(str.length()-pattern.length()); i++){
             int j;
             for( j = 0; j<pattern.length();j++){
@@ -12,8 +12,10 @@ public class PatternMatching {
             }
             if(j == pattern.length() ){
                 System.out.println("Pattern found at : "+ i);
+                return;
             }
         }
+        System.out.println("Pattern not found ....");
 
     }
 }
