1ebcc4bbcff89a4ce6dccdd9505034ec41172cd5
Sanjay Rathour <117800972+sanjayrathou@users.noreply.github.com>
2023-04-05 23:44:16 +0530

    Add files via upload

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,17 +1,31 @@
+
+// if cosider 1 is not prime.
+
 import java.util.*;
-public class PrimeNumber {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("enter the n :");
-        int n = sc.nextInt();
-        for(int i = 2; i <= n/2 ; i++){
-            if(n % i == 0){
-                System.out.println(n + " is non prime ");
-                return;
-            }
-        }
-        System.out.println(n + " is prime");
 
+public class PrimeNumber{
 
-    }
+   public static void main(String[] args){
+   
+   Scanner sc = new Scanner (System.in);
+   System.out.println("Enter the number :");
+   int n = sc.nextInt();
+   int count = 0;
+
+   for(int i = 1; i <= n; i++ ){
+
+    
+      if(n % i == 0){
+        count++;
+      }
+    
+   }
+   if(count == 2){
+    System.out.println("prime");
+   }
+   else{
+    System.out.println("not prime");
+
+   }
+}
 }
