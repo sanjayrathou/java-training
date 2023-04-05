c0831100eef9863670ae4e91b6f2cdcc8d8172b8
Sanjay Rathour <117800972+sanjayrathou@users.noreply.github.com>
2023-04-06 00:06:16 +0530

    Add files via upload

---
+++ PrimeNumber2.java
@@ -0,0 +1,20 @@
+//  if 1 is considered as a prime number.
+
+import java.util.*;
+public class PrimeNumber2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the n :");
+        int n = sc.nextInt();
+        for(int i = 2; i <= n/2 ; i++){
+            if(n % i == 0){
+                System.out.println(n + " is non prime ");
+                return;
+            }
+        }
+
+        System.out.println(n + " is prime");
+
+
+    }
+}
