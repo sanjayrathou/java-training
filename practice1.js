657ef14cd498dd1f7d27e7b18cff6fd97734c336
Sanjay Rathour <117800972+sanjayrathou@users.noreply.github.com>
2023-04-15 00:31:15 +0530

    Add files via upload

---
+++ practice1.js
@@ -0,0 +1,10 @@
+let a = "sanjay"
+let b = 45
+console.log(a+b)
+console.log(typeof (a+b))
+const c = {name : "Sanjay", section : 1, isPrincipal : false}   // object
+c ["name"] = null
+console.log(c)
+c ["name"] = "Rathour"
+// c ["name"] = 3
+console.log(c)
