8b856b6ce648e1db07521ccdd82415961d35af1d
Sanjay Rathour <117800972+sanjayrathou@users.noreply.github.com>
2023-04-14 21:39:31 +0530

    Add files via upload

---
+++ datatype.js
@@ -0,0 +1,13 @@
+/* data types in javascript nnbbsu
+   var is a globally scoped while let and const are blocks code
+*/
+
+
+let a = 34         
+let b = null
+var c = BigInt("68")
+let d = false
+var e = "Sanjay"
+let g
+console.log(a,b,c,g,d)
+console.log(typeof b)
