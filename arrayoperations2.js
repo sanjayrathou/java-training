ffb9a45b5d2ac7d6f7531d60d66bb3e9e8a0c98a
Sanjay Rathour <117800972+sanjayrathou@users.noreply.github.com>
2023-05-02 23:48:04 +0530

    Add files via upload

---
+++ arrayoperations2.js
@@ -0,0 +1,15 @@
+var arr = Array("delhi","mumbai","ghaziabad","Kolkata");
+var arr2 = Array("dd","banglore","punjab","gt");
+
+console.log(arr.concat(arr2));
+
+console.log(arr.splice(1,2)); //(startindex,number)
+console.log(arr);
+console.log(arr.indexOf("Kolkata"));
+console.log(arr.join("&&"));
+console.log(arr.reverse());
+
+
+
+
+
