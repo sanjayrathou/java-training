bb2ec22fe9127a47e8f545aca544e307c1b22930
Sanjay Rathour <117800972+sanjayrathou@users.noreply.github.com>
2023-09-11 13:49:44 +0530

    Add files via upload

--- MaxElement.java
+++ MaxElement.java
@@ -1,34 +1,24 @@
-
-import java.util.*;
+package college;
 
 public class MaxElement {
-    public static void main(String[] args) {
+	public static int  findMax(int[] arr) {
+		int max = arr[0];
+		for(int i = 1; i < arr.length; i++) {
+			if(arr[i] > max) {
+				max = arr[i];
+			}
+		}
+		
+		return max;
 
-        Scanner sc = new Scanner(System.in);
+		}
+	
+	public static void main(String[] args) {
+		int[] arr = {23,5,67,-405,34,-5};
+	System.out.println(findMax(arr)); 
+		
+	}
 
-        int size = sc.nextInt();
-
-        int[] arr = new int[size];
-
-        for (int i = 0; i < size; i++) {
-            arr[i] = sc.nextInt();
-        }
-        int max=Integer.MIN_VALUE;
-
-        for (int i=0; i<arr.length; i++){
-
-            if(max<arr[i]){
-                max=arr[i];
-            }
-
-        }
-        System.out.println("maximum element is: "+max);
-
- //       System.out.println(Integer.MIN_VALUE);     -2147483648
-
- //       System.out.println(Integer.MAX_VALUE);      2147483647
-
-
-
-    }
 }
+
+
