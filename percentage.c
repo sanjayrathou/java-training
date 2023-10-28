4f3e2f998d87033de1e8a016c95b90f345f89c9a
Sanjay Rathour <117800972+sanjayrathou@users.noreply.github.com>
2023-10-28 07:55:17 +0530

    Add files via upload

---
+++ percentage.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+int main(){
+    float m1 = 100;
+    float m2 = 94;
+    float m3 = 97;
+    float m4 = 67;
+    float p = (m1 + m2 + m3 + m4 ) / 4;
+    printf("Percentage is : %f",p);
+    return 0;
+}
