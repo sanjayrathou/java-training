d44b17055fb8c3bb487b58a7cfc9b9145d810c15
Sanjay Rathour <117800972+sanjayrathou@users.noreply.github.com>
2023-11-05 06:51:05 +0530

    Add files via upload

---
+++ String/Password.java
@@ -0,0 +1,15 @@
+package Strings;
+
+public class Password {
+
+    public static void main(String[] args) {
+
+        char[] a1 ={'s','a','n','j','a','y'};
+
+        String a2 = "sanjay";
+
+        System.out.println(a1);        // IT WILL PRINT THE REFERENCE ADDRESS OF a1.
+
+        System.out.println(a2);
+    }
+}
