89ef300bd5eb603d17e00fae4774f4dfca211e11
Sanjay Rathour <117800972+sanjayrathou@users.noreply.github.com>
2023-11-07 12:41:20 +0530

    Add files via upload

---
+++ RemoveRepeat.java
@@ -0,0 +1,14 @@
+package Strings;
+
+public class RemoveRepeat {
+
+//    static StringBuilder removeChar(StringBuilder sb, int k){
+//
+//    }
+
+    public static void main(String[] args) {
+       String str = "abbcdd";
+       StringBuilder sb = new StringBuilder(str);
+       System.out.println(sb);
+    }
+}
