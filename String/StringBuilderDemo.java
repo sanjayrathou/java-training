257b527c0affd0e566743478b9e6557235fa6bbe
Sanjay Rathour <117800972+sanjayrathou@users.noreply.github.com>
2023-11-21 08:05:01 +0530

    Add files via upload

---
+++ String/StringBuilderDemo.java
@@ -0,0 +1,19 @@
+package Strings;
+
+public class StringBuilderDemo {
+
+    public static void main(String[] args) {
+
+        StringBuilder sb = new StringBuilder("sanjay");
+
+        System.out.println(sb.length());
+        System.out.println(sb.capacity());
+        System.out.println(sb.append("rathour"));
+
+    }
+
+}
+
+
+
+
