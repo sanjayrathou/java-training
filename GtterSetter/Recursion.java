ef360d77d3206d3b04de156fac795de18d972a6b
Sanjay Rathour <117800972+sanjayrathou@users.noreply.github.com>
2024-11-20 15:02:19 +0530

    Update Recursion.java

--- GtterSetter/Recursion.java
+++ GtterSetter/Recursion.java
@@ -4,7 +4,6 @@ public class Recursion {
     static int getResult(int n) {
         if(n == 0) return 0;
         if(n == 1) return 1;
-
         int ans = getResult(n-1) + getResult(n-2);
         return ans;
     }
